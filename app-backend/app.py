03bcfb7ad70e5b27adb0b27a8e9c4012ae2d7d44
Zi Hao LAM <lamzihao98@gmail.com>
2023-04-09 06:09:51 +0800

    add frontend and backend

--- app-backend/app.py
+++ app-backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from utils import get_ip, get_utilization
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+cors = CORS(app)
 
 @app.route("/ping", methods=["GET"])
 def ping():
