e1f0d3e0c219f28c18bf3da965f954d7c7fe9837
Lam Zi Hao <lamzi@amazon.com>
2023-04-27 11:40:40 +0800

    add private ip in dead response healtcheck

--- app-backend/utils.py
+++ app-backend/utils.py
@@ -39,18 +39,19 @@ async def fetch_health(url: str, session: ClientSession, **kwargs) -> tuple:
 node_mapping = {}
 
 
-async def async_fetch(url):
+async def async_fetch(url, ip):
     try:
         async with aiohttp.ClientSession() as session, async_timeout.timeout(2):
             async with session.get(url) as response:
                 json_response = await response.json()
-                node_mapping[url] = json_response["node_name"]
+                node_mapping[url] = [json_response["node_name"], ip]
                 return json_response
     
     except Exception as e:
         logging.error(e)
         return {
-            "node_name": node_mapping[url],
+            "node_name": node_mapping[url][0],
+            "private_ip": node_mapping[url][1],
             "utilization": {
                 "cpu": -1,
                 "memory": -1,
@@ -62,4 +63,4 @@ loop = asyncio.get_event_loop()
 
 def get_all_nodes_health():
     node_ips = ["10.0.3.10", "10.0.4.10", "10.0.5.10", "10.0.3.11", "10.0.3.80", "10.0.4.80", "10.0.1.11", "10.0.1.10", "10.0.3.100", "10.0.4.100", "10.0.3.120", "10.0.4.120", "10.0.1.45", "10.0.1.46"]
-    return loop.run_until_complete(asyncio.gather(*[async_fetch(f"http://{node_ip}:8020/health") for node_ip in node_ips]))
+    return loop.run_until_complete(asyncio.gather(*[async_fetch(f"http://{node_ip}:8020/health", node_ip) for node_ip in node_ips]))
